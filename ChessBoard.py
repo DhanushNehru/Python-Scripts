7c0213ffc4b630d0e334cb13d74bb575ab945d54
dhanushn@vuram.com <dhanushn@vuram.com>
2021-04-17 19:47:45 +0530

    Chessboard using matplotlib

---
+++ ChessBoard.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LogNorm
+
+dx, dy = 0.015, 0.05
+#numpy.arrange returns evenly spaced values within a given interval
+x = np.arange(-04.0, 4.0, dx)
+y = np.arange(-04.0, 4.0, dy)
+#returns coordinate matrices from coordinate vectors
+X, Y = np.meshgrid(x, y)
+
+extent = np.min(x), np.max(x), np.min(y), np.max(y)
+
+Z1 = np.add.outer(range(8), range(8)) % 2
+
+plt.imshow(Z1, cmap="binary_r", interpolation="nearest", extent =  extent, alpha=1)
+
+plt.title("Chess Board", fontweight="bold")
+
+plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# BeginnersProgram
+# Python Scripts
+
+# Script 1
+
+Code using turtle graphics
+TurtleGraphics.py
+
+# Script 2
+
+Displaying a popup window
+DisplayPopupWindow.py
+
+# Script 3
+
+Python code to take a break while working long hours
+TakeABreak.py
+
+# Script 4
+
+Create a chesboard using matplotlib
+ChessBoard.py

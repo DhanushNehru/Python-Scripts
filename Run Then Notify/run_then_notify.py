63baa0c0b2435c401ac1451a4668ed1af6b788e0
rohan843 <rohan.sharma2934@gmail.com>
2023-10-06 02:05:44 +0530

    finish basic logic

--- Run Then Notify/run_then_notify.py
+++ Run Then Notify/run_then_notify.py
@@ -1,23 +1,30 @@
 import subprocess
 import sys
 import time
+import socket
 
 
 def run_then_notify(command):
     try:
         start_time = time.time()
         completed_process = subprocess.run(
-            command, text=True, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT, stdin=sys.stdin)
+            command,
+            text=True,
+            shell=True,
+            stdout=sys.stdout,
+            stderr=subprocess.STDOUT,
+            stdin=sys.stdin
+        )
 
         end_time = time.time()
         execution_time = end_time - start_time
 
-        print('-' * 50)
-
-        print(f"Command '{command}' completed with exit code {completed_process.returncode}.")
-        print(f"Execution time: {execution_time:.2f} seconds")
-        print("Standard Output:")
-        print(completed_process.stdout.strip().split('\n'))
+        notif_subject = '[Run completed] Your command finished execution'
+        notif_message = f"Command '{command}' completed\n\
+            \twith exit code {completed_process.returncode}\n\
+            \tin {execution_time:.2f} seconds\n\
+            \n\
+            This is an automated message sent from your device {socket.gethostname()}"
 
     except Exception as e:
         print(f"Error: {e}")

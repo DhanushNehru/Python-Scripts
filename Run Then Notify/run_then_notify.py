bc87acbda9699f895a0419f812278a5c9f96d447
rohan843 <rohan.sharma2934@gmail.com>
2023-10-06 01:32:27 +0530

    update std streams
    Now, the running process can be interactive, i.e., user inputs through
    the terminal are now supported. This will be useful in cases where a
    long-running file needs some user inputs first, before beginning the
    major execution.

--- Run Then Notify/run_then_notify.py
+++ Run Then Notify/run_then_notify.py
@@ -2,22 +2,26 @@ import subprocess
 import sys
 import time
 
+
 def run_then_notify(command):
     try:
         start_time = time.time()
-        completed_process = subprocess.run(command, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        completed_process = subprocess.run(
+            command, text=True, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT, stdin=sys.stdin)
+
         end_time = time.time()
         execution_time = end_time - start_time
 
-        # Print the captured stdout
-        print(f"Command '{command}' completed.")
+        print('-' * 50)
+
+        print(f"Command '{command}' completed with exit code {completed_process.returncode}.")
         print(f"Execution time: {execution_time:.2f} seconds")
-        print("Exit Code:", completed_process.returncode)
         print("Standard Output:")
         print(completed_process.stdout.strip().split('\n'))
 
     except Exception as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

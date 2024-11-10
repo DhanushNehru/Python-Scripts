2718f80f764f956cb0df173fc6c75dd2d94758bd
Badis <bazizbadis13@gmail.com>
2024-11-10 11:57:38 +0100

    Add reconnection attempts for Wi-Fi

--- Auto WiFi Check/wifi_checker.py
+++ Auto WiFi Check/wifi_checker.py
@@ -20,12 +20,31 @@ def job():
     if subprocess.call("netsh interface set interface Wi-Fi enabled") == 0:
         print("WiFi is enabled and connected to internet")
         hostname = "www.google.com"
-        response = subprocess.call("ping -n 1 " + hostname)
+        response = subprocess.call(f"ping -n 1 {hostname}") # Using f-string for cleaner string formatting
         if response == 1:
             print("Your Connection is not working")
-            disable()
-            time.sleep(1)
-            enable()
+
+            attempt_counter = 0
+            max_attempts = 3
+            
+            while attempt_counter < max_attempts:
+                print(f"Attempt {attempt_counter} to reconnect...")
+
+                disable()
+                time.sleep(1)
+                enable()
+
+                time.sleep(5)
+
+                response = subprocess.call(f"ping -n 1 {hostname}")
+                if response == 0:
+                    print("Reconnection successful!")
+                    break
+                elif response == 1:
+                    print(f"Reconnection attempt {attempt_counter} failed.")
+                
+                if attempt_counter == max_attempts and response != 0:
+                    print(f"Failed to reconnect after {attempt_counter} attemps.")
 
 def is_admin():
     try:

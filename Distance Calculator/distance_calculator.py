f8f99cce88af92c0f17f21934d853f3759acfab8
Jaiwant <158043502+Mathdallas-code@users.noreply.github.com>
2025-01-16 16:31:56 +0530

    Added better input validation

--- Distance Calculator/distance_calculator.py
+++ Distance Calculator/distance_calculator.py
@@ -6,39 +6,66 @@
 # So, according to pythagorean theorem, the distance between these two points is 5.0.
 
 # Hope this helps!
+
+# Importing module(s)
 import math
 
+# Main loop
 while True:
-    que=input("Do you want to calculate the distance between two points? (yes/no): ")
-    if que.lower() == "yes":
+    use_program=input("Do you want to calculate the distance between two points? (yes/no): ")
+    if use_program.lower() == "yes":
         pass
-    elif que.lower() == "no":
+    elif use_program.lower() == "no":
         print("Thank you for using the distance calculator!")
         quit()
     else:
         print("Invalid input! Please enter 'yes' or 'no'.")
         continue    
 
-    startX = input("Enter starting x-coordinate: ")
-    if startX.isalpha():
-        print("Error! Input has to be a number!")
-        continue
-    startY = input("Enter starting y-coordinate: ")
-    if startY.isalpha():
-        print("Error! Input has to be a number!")
-        continue
-    endX = input("Enter ending x-coordinate: ")
-    if endX.isalpha():
-        print("Error! Input has to be a number!")
-        continue
-    endY = input("Enter ending y-coordinate: ")
-    if endY.isalpha():
-        print("Error! Input has to be a number!")
-        continue
+    # Input validation for startX, startY, endX, endY
 
+    # startX
+    while True:
+        startX = input("Enter starting x-coordinate: ")
+        if not startX.isnumeric():
+            print("Error! Input has to be a number!")
+            continue
+        else:
+            break
+    
+    # startY
+    while True:
+        startY = input("Enter starting y-coordinate: ")
+        if not startY.isnumeric():
+            print("Error! Input has to be a number!")
+            continue
+        else:
+            break
+
+    # endX
+    while True:
+        endX = input("Enter ending x-coordinate: ")
+        if not endX.isnumeric():
+            print("Error! Input has to be a number!")
+            continue
+        else:
+            break
+
+    # endY    
+    while True:
+        endY = input("Enter ending y-coordinate: ")
+        if not endY.isnumeric():
+            print("Error! Input has to be a number!")
+            continue
+        else:
+            break
+
+    # Converting the values to floats
     startX = float(startX)
     startY = float(startY)
     endX = float(endX)
     endY = float(endY)
+
+    # The calculation
     distance = math.sqrt(math.pow(startX - endX, 2) + math.pow(startY - endY, 2))
-    print(f"The distance between ({startX}, {startY}) and ({endX}, {endY}) is {distance}.")
+    print(f"The distance between ({startX}, {startY}) and ({endX}, {endY}) is {distance} units.")

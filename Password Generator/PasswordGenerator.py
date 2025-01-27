3d7ba92511cd86d00ec73c53ee5dd3d21ce05782
George <dsg1varna@gmail.com>
2025-01-27 17:03:43 +0200

    Update PasswordGenerator.py (#395)
    * Update PasswordGenerator.py
    
    * Update
    
    With these enhancements, the application will offer greater flexibility, functionality, and a better user experience.
    
    * update
    
    Now the user can specify how many profiles they want to generate and the user is prompted to decide whether they want to save the profiles to a file.

--- Password Generator/PasswordGenerator.py
+++ Password Generator/PasswordGenerator.py
@@ -1,96 +1,87 @@
 import random as rr
 import string as ss
 
-
-
-"""
-ASCII 
-A -> Z : 65 -> 90
-a -> z : 97 -> 122
-"""
-
 characters = ['@', '#', '$', '%', '&', '?']
 
 
-pass_len = int(input('How lengthy do you want your password to be : '))
+def generate_password(pass_len):
+    # Initialize counters
+    total_nums = 0
+    total_symbols = 0
+    total_cap = 0
+    total_low = 0
 
-tempy, tempz = 0, 0
-tempx = rr.randint(2, pass_len-1)                           # alphabets
+    # Ensure at least one of each type
+    tempx = rr.randint(2, max(2, pass_len - 2))  # at least 2 letters
+    remaining = pass_len - tempx
 
-if tempx != pass_len:
-    tempy = rr.randint(1, (pass_len - tempx - 1))           # numbers
+    tempy = rr.randint(1, max(1, remaining - 1))  # at least 1 number
+    remaining -= tempy
     total_nums = tempy
 
-if (tempx + tempy) != pass_len:
-    tempz = rr.randint(1, (pass_len-(tempx+tempy)))         # special characters
+    tempz = remaining  # rest goes to special characters
     total_symbols = tempz
 
-# password : empty string for now
-pass_word = ''
+    # Generate password
+    pass_word = ''
 
-# adding alphabets
-
-while tempx:
-    x = tempx
-    num_cap = rr.randint(0, x)
-    num_low = x-num_cap
-
+    # Add alphabets
+    num_cap = rr.randint(1, tempx - 1)  # at least 1 uppercase
+    num_low = tempx - num_cap  # rest lowercase
     total_cap = num_cap
     total_low = num_low
 
-    # capitals in password :
-    while num_cap:
-        temp = chr(rr.randint(65, 90))
-        pass_word = pass_word + str(temp)
-        num_cap -= 1
+    # Add capitals
+    pass_word += ''.join(chr(rr.randint(65, 90)) for _ in range(num_cap))
 
-    # lower-case in password :
-    while num_low:
-        temp = chr(rr.randint(97, 122))
-        pass_word = pass_word + str(temp)
-        num_low -= 1
+    # Add lowercase
+    pass_word += ''.join(chr(rr.randint(97, 122)) for _ in range(num_low))
 
-    break
+    # Add numbers
+    pass_word += ''.join(str(rr.randint(0, 9)) for _ in range(tempy))
 
-# adding numbers to the password
-while tempy:
-    temp = (rr.randint(0, 9))
-    pass_word = pass_word + str(temp)
-    tempy -= 1
+    # Add special characters
+    pass_word += ''.join(rr.choice(characters) for _ in range(tempz))
 
-# adding special characters to the password
-while tempz:
-    temp = rr.randint(0, len(characters)-1)
-    pass_word = pass_word + characters[temp]
-    tempz -= 1
+    return pass_word, total_cap, total_low, total_nums, total_symbols
 
-#shuffles the string
+
 def shuffle_(alpha):
     str_temp = list(alpha)
     rr.shuffle(str_temp)
-    alpha = ''.join(str_temp)
-    return alpha
+    return ''.join(str_temp)
 
-#adds colour to the text
+
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
-final_pass =colored(200,200,50, (shuffle_(shuffle_(shuffle_(pass_word)))))
 
+def main():
+    pass_len = int(input('How lengthy do you want your password to be : '))
 
-# result & summary
-result = """
-Generate Password Summary :
+    if pass_len < 4:
+        print("Password length must be at least 4 characters")
+        return
 
-Charactor Uppercase : {0}
-Charactor Lowercase : {1}
+    pass_word, total_cap, total_low, total_nums, total_symbols = generate_password(pass_len)
+
+    # Shuffle multiple times
+    final_pass = colored(200, 200, 50, shuffle_(shuffle_(shuffle_(pass_word))))
+
+    result = """
+Generate Password Summary:
+
+Character Uppercase : {0}
+Character Lowercase : {1}
 Numbers : {2}
 Symbols : {3}
 
-Your computer generated password is :
+Your computer generated password is:
 {4}
 """.format(total_cap, total_low, total_nums, total_symbols, final_pass)
 
-print(result)
+    print(result)
 
-# print(f"\nYour computer generated password is : {final_pass}\n\n")
+
+if __name__ == "__main__":
+    main()

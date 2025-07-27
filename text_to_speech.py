6750246c6495b90855b3fe0388ed622841dd3e36
L. Abhilash <160405976+LAbhilashKumar@users.noreply.github.com>
2025-07-27 13:33:29 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,13 +1,13 @@
 import pyttsx3
+default_SpeechRate= 176
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+for voice in voices:
+    print(voice.id)
+    print(voice.name)
 
-engine =pyttsx3.init()
-voices= engine.getProperty("voices")
-for i in voices:
-    print(i.id)
-    print(i.name)
-
-id ="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
-engine.setProperty(i, id)
-engine.setProperty("rate",176)
+desired_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
+engine.setProperty(voice, desired_voice_id)
+engine.setProperty("rate",default_SpeechRate)
 engine.say("hello world")
 engine.runAndWait()

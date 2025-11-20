ba3595272d083911966852ace44a277d6dcd107d
javi0martinez <j.martinezsanchez1999@gmail.com>
2025-11-20 23:01:00 +0100

    Get parties names and votes

--- Dhondt calculator/main.py
+++ Dhondt calculator/main.py
@@ -9,6 +9,14 @@ num_seats = get_positive_integer("Enter the number of seats to allocate:")
 ## Number of parties
 num_parties = get_positive_integer("Enter the number of parties:")
 
+## Names and votes of each party
+for i in range(num_parties):
+    party_name = input(f"Enter the name of party {i + 1}: ")
+    party_votes = get_positive_integer(f"Enter the number of votes for {party_name}: ")
+    results.append((party_name, party_votes))
+
+
+# Calculate the seats
 
 
 # Print the result of seats
